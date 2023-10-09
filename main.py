e09ddbbf007b58a8931b0972f7669ea8dbf565b0
Gilles <gillesvandenboom@hotmail.com>
2023-10-09 21:51:00 +0200

    requirement doc and outline
    basics

---
+++ main.py
@@ -0,0 +1,21 @@
+import pickle
+import schedule
+import time
+
+tasks = []
+
+def add_tasks(title, description, due_date):
+
+def delete_tasks(task_index):
+
+def view_tasks():
+
+def delete_tasks():
+
+def save_tasks():
+
+def load_tasks():
+
+def mark_completed(task_index):
+
+def send_notification(task_title):
